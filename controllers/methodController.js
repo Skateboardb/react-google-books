fadf4f4f2b2b28e7da24c14d34aea2fcfb3b9316
Branford Shaw <branford.shaw@gmail.com>
2020-03-10 19:07:23 -0400

    added controller files for google api and route methods

---
+++ controllers/methodController.js
@@ -0,0 +1,9 @@
+const db = require('../models');
+
+module.exports = {
+	findAll: function(req, res) {
+		db.Book.find(req.query)
+			.then(dbBook => res.json(dbBook))
+			.catch(err => res.status(422).json(err));
+	}
+};
