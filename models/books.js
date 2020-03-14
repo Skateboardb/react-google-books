b94a15a93cb4503fdada4fa08ff7047eb0a3eeb3
Branford Shaw <branford.shaw@gmail.com>
2020-03-14 12:11:39 -0400

    created mongoose book schema

---
+++ models/books.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newBook = new Schema({
+	title: String,
+	authors: [String],
+	description: String,
+	img: {
+		type: { String }
+	},
+	link: {
+		type: String
+	}
+});
+
+const books = mongoose.model('books', newBook);
+module.exports = books;
