5044918368b5c54008b8e60efe093eed68a8e700
Branford Shaw <branford.shaw@gmail.com>
2020-03-17 18:23:16 -0400

    converted search bar to bootstrap - imported and used InputGroup and FormControl

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 function SearchForm(props) {
 	return (
 		<div id="searchContainer">
 			<h3>Book Search</h3>
 			<form id="bookSearch">
-				<label htmlFor="bookInput" form="bookSearch">
-					Enter a book to search:
-				</label>
+				<label htmlFor="bookInput" form="bookSearch"></label>
 				<br></br>
-				<input
+
+				<InputGroup
+					className="mb-3"
 					type="text"
 					name="bookInput"
 					id="bookInput"
 					form="bookSearch"
 					onChange={e => props.handleChange(e)}
-					placeholder="Book Title"
+					placeholder="Enter book title to search"
 					required
-				/>
+				>
+					<InputGroup.Prepend>
+						<InputGroup.Text id="inputGroup-sizing-default">
+							Title:
+						</InputGroup.Text>
+					</InputGroup.Prepend>
+					<FormControl
+						aria-label="Default"
+						aria-describedby="inputGroup-sizing-default"
+					/>
+				</InputGroup>
 				<br></br>
 				<button type="submit" onClick={e => props.handleSearchClick(e)}>
 					Search
