2858508c6ed87ac3f3a386f89e47d08f3180353f
Branford Shaw <shaw.branford@gmail.com>
2020-05-21 10:56:02 -0500

    shifted description text to sit below title and author info; made buttons appear on hover

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -47,6 +47,18 @@ class BookResult extends React.Component {
 			});
 	}
 
+	getInitialState() {
+		return {
+		  isMouseInside: false
+		};
+	  }
+	  mouseEnter = () => {
+		this.setState({ isMouseInside: true });
+	  }
+	  mouseLeave = () => {
+		this.setState({ isMouseInside: false });
+	  }
+
 	render() {
 		return (
 			<div
@@ -54,63 +66,74 @@ class BookResult extends React.Component {
 				id={this.props.id ? this.props.id : null}
 				style={{ display: this.state.deleted ? 'none' : 'block' }}
 			>
-				<div className="row aboutBookRow">
-					<div className="aboutBook">
-						<h4>{this.props.subtitle ? this.props.title + ": " + this.props.subtitle : this.props.title} </h4>
-						<p>
-							By: {this.props.authors ? this.props.authors.join(', ') : 'N/A'}
-						</p>
-					</div>
-					<div className="btnDiv">
-						{// if link to book exists include View button else do not
-						this.props.link ? (
-							<a
-								href={this.props.link}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<button type="button" name="view">
-									View
-								</button>
-							</a>
+				
+				<div className="row">
+
+					<div className="col" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+						{this.props.img ? (
+							<img
+								src={
+									// if smallthubmail exists on this.props.img use that else if thumbnail exists on this.props.img use that else leave src empty
+									this.props.img.smallThumbnail
+										? this.props.img.smallThumbnail
+										: this.props.img.thumbnail
+										? this.props.img.thumbnail
+										: ''
+								}
+								alt="book cover"
+								// className="col"
+							/>
 						) : null}
-						{// if this.props.path is "/" display save button else display Delete button
-						this.props.path === '/' ? (
-							<button
-								type="button"
-								name="save"
-								onClick={this.handleSaveClick}
-								disabled={this.state.saved}
-							>
-								{this.state.saved ? 'Saved' : 'Save'}
-							</button>
-						) : (
-							<button
-								type="button"
-								name="Delete"
-								onClick={this.handleDeleteClick}
-								disabled={this.state.deleted}
-							>
-								Delete
-							</button>
-						)}
-					</div>
-				</div>
-				<div className="">
-					{this.props.img ? (
-						<img
-							src={
-								// if smallthubmail exists on this.props.img use that else if thumbnail exists on this.props.img use that else leave src empty
-								this.props.img.smallThumbnail
-									? this.props.img.smallThumbnail
-									: this.props.img.thumbnail
-									? this.props.img.thumbnail
-									: ''
-							}
-							alt="book cover"
-						/>
-					) : null}
-					<p>{this.props.description ? this.props.description : 'No Description Available'}</p>
+						{this.state.isMouseInside ? <div className="btnDiv">
+								{// if link to book exists include View button else do not
+								this.props.link ? (
+									<a
+										href={this.props.link}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button type="button" name="view">
+											View
+										</button>
+									</a>
+								) : null}
+								{// if this.props.path is "/" display save button else display Delete button
+								this.props.path === '/' ? (
+									<button
+										type="button"
+										name="save"
+										onClick={this.handleSaveClick}
+										disabled={this.state.saved}
+									>
+										{this.state.saved ? 'Saved' : 'Save'}
+									</button>
+								) : (
+									<button
+										type="button"
+										name="Delete"
+										onClick={this.handleDeleteClick}
+										disabled={this.state.deleted}
+									>
+										Delete
+									</button>
+								)}
+							</div> : null}
+
+					</div> 
+
+						<div className="col-10 aboutBook">
+							<div className="aboutBookRow row">
+								<h4 className="col-12">{this.props.subtitle ? this.props.title + ": " + this.props.subtitle : this.props.title} </h4>
+
+								<p className="col-12">
+									By: {this.props.authors ? this.props.authors.join(', ') : 'N/A'}
+								</p>
+							</div>
+							
+							<div className="row">
+								<p className="col">{this.props.description ? this.props.description : 'No Description Available'}</p>
+							</div>		
+						</div>
 				</div>
 			</div>
 		);
